95053673f16df2f056c7a8f16be8fb7207718d87
ifeiwuch <imiwuchukwu@gmail.com>
2024-07-08 10:22:16 -0400

    test

--- RMSE.py
+++ RMSE.py
@@ -7,5 +7,27 @@ def RMSE(predictions, targets):
 
 # Example usage
 predictions = np.array([10,20,30])
-targets = np.array([12,18,28])
+targets = np.array([12,18,30])
 print(RMSE(predictions, targets))
+
+##ADE
+def ADE(predictions, targets):
+    return np.mean(np.abs(predictions - targets))
+
+# Example usage
+predictions = np.array([10,20,30])
+targets = np.array([12,18,30])
+print(ADE(predictions, targets))
+
+
+##FDE
+def FDE(fpositions, tpositions):
+    return np.mean(np.abs(fpositions - tpositions))
+
+
+##Negative Log Likelihood
+
+##def
+
+
+##KL Divergence
